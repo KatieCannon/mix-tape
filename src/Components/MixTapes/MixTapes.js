9e9767b7955680b4b8045d2b6e38f6d7b32293cc
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-22 14:41:54 +0100

    setting up mixtapes components

---
+++ src/Components/MixTapes/MixTapes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MixTapes.css';
+
+class MixTapes extends React.Component {
+    render(){
+        return <h1>mixTapes</h1>
+    }
+}
+
+export default MixTapes;
