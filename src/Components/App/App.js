350ffd56be5090e4cabc30193cdf74d5cf23182f
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-23 18:09:38 +0100

    more data hardcoded

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,10 +11,49 @@ class App extends React.Component {
         this.state = {
             mixTapes:[{
                 id: 1,
-                name: 'mixTape1'
+                name: 'mixTape1 r&b'
             }, {
                 id: 2,
                 name: 'mixTape2'
+            }, {
+                id: 3,
+                name: 'mixTape3'
+            }, {
+                id:4,
+                name: 'mixTape4 reggae'
+            }, {
+                id:5,
+                name: 'mixTape5'
+            }, {
+                id: 6,
+                name: 'mixTape6'
+            }, {
+                id: 7,
+                name: 'mixTape7'
+            }, {
+                id: 8,
+                name: 'mixTape8'
+            }, {
+                id: 9,
+                name: 'mixTape9'
+            }, {
+                id: 10,
+                name: 'mixTape10'
+            }, {
+                id: 11,
+                name: 'mixTape11'
+            }, {
+                id: 12,
+                name: 'mixTape12'
+            }, {
+                id: 13,
+                name: 'mixTape13'
+            }, {
+                id: 14,
+                name: 'mixTape14'
+            }, {
+                id: 15,
+                name: 'mixTape15'
             }]
         }
     }
