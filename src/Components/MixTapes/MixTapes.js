a2792c2f281567393a34d43199d2914f81dfad88
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-23 18:20:58 +0100

    mixtapes styling and passing of data

--- src/Components/MixTapes/MixTapes.js
+++ src/Components/MixTapes/MixTapes.js
@@ -5,9 +5,11 @@ import MixTape from '../MixTape/MixTape';
 class MixTapes extends React.Component {
     render(){
         return (
-            <div>
-                {this.props.mixTapes.map(mix =>{
-                    return <MixTape key={mix.id} name={mix.name}/>})}
+            <div className='mixTapes'>
+                {this.props.mixTapes.map(mix => {
+                    return <MixTape key={mix.id} name={mix.name} />
+                    
+                })}
             </div>
         )
     }
