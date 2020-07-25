b2cbe0170415454c13c9e8654339850cf4769abf
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-25 17:44:16 +0100

    selected tracks displayed in mixtape

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './PlayList.css';
+import Tracklist from '../TrackList/TrackList';
 
 class PlayList extends React.Component {
     render(){
         return (
             <div className='PlayList'>
                 <h1>PlayList</h1>
-                {this.props.mixTapeTracks.map(track => {
-                    console.log(track);
-                    return <h1>{track.name}</h1>
-                })}
+                <Tracklist tracks={this.props.mixTapeTracks}/>
                 </div>
         )
     }
