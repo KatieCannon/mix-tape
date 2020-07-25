37c0afa81b19ae973bf3c9bb1063dbcf7abbf722
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-25 13:32:05 +0100

    search functionality

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    handleTermChange(event) {
+        this.setState({
+            term: event.target.value
+        });
+    }
     render(){
         return (
             <div className='SearchBar'>
-                <input placeholder='Enter a Song, Album, or Artist'/>
+                <input placeholder='Enter a Song, Album, or Artist' onChange={this.handleTermChange}/>
                 <button className='SearchButton'>SEARCH</button>
             </div>
         )
