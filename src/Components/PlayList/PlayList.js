7abd53d010fec3a60b96451da335989055ede72a
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-25 19:26:28 +0100

    setting mixtape name

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -3,10 +3,17 @@ import './PlayList.css';
 import Tracklist from '../TrackList/TrackList';
 
 class PlayList extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    handleNameChange(event){
+        this.props.onNameChange(event.target.value);
+    }
     render(){
         return (
             <div className='PlayList'>
-                <h1>PlayList</h1>
+                 <input placeholder="mixTape name" onChange={this.handleNameChange}/>
                 <Tracklist tracks={this.props.mixTapeTracks} onRemove={this.props.onRemove} isRemoval={true} />
                 </div>
         )
