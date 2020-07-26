700bcc79844918cf02050664b3eea83b20919551
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-26 19:54:53 +0100

    redirecturi

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'http://mix-tape.netlify.app';
 let accessToken;
 const Spotify = {
     getAccessToken(){
