54bbae917a4828dd8f7274bb38a17f358a5d3792
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-27 17:02:36 +0100

    validation on hover of disabled button

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -5,9 +5,12 @@ import Tracklist from '../TrackList/TrackList';
 class PlayList extends React.Component {
     constructor(props){
         super(props);
+        this.state={isHovering: false}
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleSaveMixTape = this.handleSaveMixTape.bind(this);
         this.canMixTapeBeSaved = this.canMixTapeBeSaved.bind(this);
+        this.handleHover = this.handleHover.bind(this);
+        this.toggleMouseHover = this.toggleMouseHover.bind(this);
     }
     handleNameChange(event){
         this.props.onNameChange(event.target.value);
@@ -22,6 +25,16 @@ class PlayList extends React.Component {
             this.props.onSave();
         }
     }
+    handleHover() {
+        this.setState({
+            isHovering: true
+        });
+    }
+    toggleMouseHover(){
+        this.setState({
+        isHovering: !this.state.isHovering
+        });
+    }
     render(){
         const mixTapeName = this.props.mixTapeName;
         const isEnabled = mixTapeName.length > 0;
@@ -29,8 +42,9 @@ class PlayList extends React.Component {
             <div className='PlayList'>
                  <input value={this.props.mixTapeName} placeholder="mixTape name" onChange={this.handleNameChange}/>
                 <Tracklist tracks={this.props.mixTapeTracks} onRemove={this.props.onRemove} isRemoval={true} />
-                <div className="Playlist-save-div">
+                <div className="Playlist-save-div" onMouseOver={this.handleHover} onMouseLeave={this.toggleMouseHover} onKeyDown={this.handleHover} onKeyUp={this.toggleMouseHover}>
                     <button className="Playlist-save" onClick={this.handleSaveMixTape} disabled={!isEnabled}>SAVE TO SPOTIFY</button>
+                    {!isEnabled && this.state.isHovering &&<div className='tooltip'>A mixTape name must be entered</div>}
                 </div>
             </div>
         )
